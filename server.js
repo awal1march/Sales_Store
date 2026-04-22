5af490770de313e1def9330443955cfee7325e5e
awal1march <awal1march@gmail.com>
2026-04-22 16:15:53 +0000

    Fix port for Render deployment

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.static("public"));
 const paymentRoutes = require("./routes/paymentRoutes");
 app.use("/payments", paymentRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
